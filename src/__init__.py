28bfead6cbb44d442bda827a5cb4ceb690df3fcc
codec <191799163+codec266@users.noreply.github.com>
2026-04-18 18:30:57 +0800

    feat: integrate bulk tools and dynamic mapping into UI

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,8 @@ from PyQt6.QtWidgets import QMenu
 from aqt import mw, gui_hooks
 from aqt.utils import showText, tooltip
 from .service import process_note
-
+from .bulk import bulk_add, bulk_remove, find_empty_pos, reset_mappings
+from .mapping import resolve_mappings
 
 def editor_button(buttons, editor):
     """ add japanese part of speech button to editor (品詞追加)"""
@@ -22,15 +23,18 @@ def add_pos(editor):
     if not note:
         return
 
-    editor.mw.checkpoint("Add POS Tag")
+    mappings = resolve_mappings(note.model(), editor.widget)
+    if not mappings:
+        return
 
-    success, msg = process_note(note)
+    success, msg = process_note(note, mappings["expr"], mappings["read"], mappings["pos"])
 
     if success:
-        note.flush()
+        if note.id != 0:
+            note.flush()
+
         editor.loadNote()
 
-    # debug
     if msg:
         tooltip(msg)
 
@@ -41,24 +45,19 @@ def about_dialog():
         title='About',
     )
 
-
-def bulk_add():
-    pass
-
-
-def bulk_remove():
-    pass
-
-
 # add menu items
 pos_menu = QMenu('Part of Speech', mw)
 pos_menu_add = pos_menu.addAction('bulk add')
 pos_menu_remove = pos_menu.addAction('bulk remove')
+pos_menu_find = pos_menu.addAction('find empty')
+pos_menu_reset = pos_menu.addAction('reset mappings')
 pos_menu_about = pos_menu.addAction('about')
 
 # triggers
 pos_menu_add.triggered.connect(bulk_add)
 pos_menu_remove.triggered.connect(bulk_remove)
+pos_menu_find.triggered.connect(find_empty_pos)
+pos_menu_reset.triggered.connect(reset_mappings)
 pos_menu_about.triggered.connect(about_dialog)
 
 # add to tools menu
